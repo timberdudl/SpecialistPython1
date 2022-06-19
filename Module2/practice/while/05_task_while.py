316ab6c4b2da4c20e20d847195c058c41729c50b
timberdudl <106873861+timberdudl@users.noreply.github.com>
2022-06-19 17:19:29 +0300

    Update 05_task_while.py

--- Module2/practice/while/05_task_while.py
+++ Module2/practice/while/05_task_while.py
@@ -17,4 +17,25 @@
 # #
 
 
-# TODO: your code here
+square_size = int(input('введите длину стороны квадрата: '))
+num_pre_spaces = -1
+num_betw_spaces = square_size
+if square_size % 2 == 0:
+    while num_betw_spaces > 0:
+        num_betw_spaces -= 2
+        num_pre_spaces += 1
+        print(' ' * num_pre_spaces, '#', ' ' * num_betw_spaces, '#', sep = '')
+    while num_betw_spaces <= square_size - 2:
+        print(' ' * num_pre_spaces, '#', ' ' * num_betw_spaces, '#', sep = '')
+        num_pre_spaces -= 1
+        num_betw_spaces += 2
+if square_size % 2 == 1:
+    while num_betw_spaces > 1:
+        num_betw_spaces -= 2
+        num_pre_spaces += 1
+        print(' ' * num_pre_spaces, '#', ' ' * num_betw_spaces, '#', sep = '')
+    print(' ' * (square_size // 2), '#', sep = '')
+    while num_betw_spaces <= square_size - 2:
+        print(' ' * num_pre_spaces, '#', ' ' * num_betw_spaces, '#', sep = '')
+        num_pre_spaces -= 1
+        num_betw_spaces += 2
