a9e73833f373daf6339b23e00c2b626a539916de
timberdudl <106873861+timberdudl@users.noreply.github.com>
2022-06-25 12:49:22 +0300

    Update 03_task_repeat.py

--- Module5/practice/repeat/03_task_repeat.py
+++ Module5/practice/repeat/03_task_repeat.py
@@ -1,5 +1,13 @@
 # Найдите количество чисел являющихся палиндромами в диапазоне от a до b включительно
 # Известно, что a и b целые положительные числа.
 
-a = ...
-b = ...
+def palindrome_quantity(up=0, to=1000):
+    counter = 0
+    for number in range(up, to):
+        number = str(number)
+        if number == number[::-1]:
+            counter += 1
+    return counter
+
+
+print(palindrome_quantity(10, 1000))
